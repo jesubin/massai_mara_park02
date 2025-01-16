ab76cd3b6631e1594816564bad9c41afd627de78
myname <dokdokorea01@naver.com>
2025-01-16 11:58:26 +0900

    java/src 추가

---
+++ src/main/java/subin/spring/massai_mara_park/MassaiMaraPark02Application.java
@@ -0,0 +1,13 @@
+package subin.spring.massai_mara_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassaiMaraPark02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassaiMaraPark02Application.class, args);
+	}
+
+}
